95e408f9632d544b58daaf83efff8de638aefb1f
Yitz <dlefkowitz@lightricks.com>
2024-06-19 22:08:41 +0300

    Configure jar.

--- demo-project/java-compile/build.gradle.kts
+++ demo-project/java-compile/build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
     java
 }
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "com.lightricks.java_sample.App"
+        )
+    }
+    archiveBaseName.set("app")
+    destinationDirectory.set(file("${buildDir}/libs/"))
+}
