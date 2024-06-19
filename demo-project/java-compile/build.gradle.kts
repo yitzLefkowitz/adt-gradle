2e9b4c8de3fb0f119c12a7c06fe6826f556fb455
Yitz <dlefkowitz@lightricks.com>
2024-06-19 22:05:49 +0300

    Create java compile solution.

--- demo-project/java-compile/build.gradle.kts
+++ demo-project/java-compile/build.gradle.kts
@@ -7,7 +7,15 @@ abstract class JavaCompileTask : DefaultTask() {
 
     @TaskAction
     fun compile() {
-
+        val javaFiles = inputDirLocation.walkTopDown()
+            .filter { it.isFile && it.extension == "java" }
+            .map { it.absolutePath }
+            .toList()
+            .toTypedArray()
+        println("Compiling")
+        project.exec {
+            commandLine("javac", "-d", outputDirectory.absolutePath, *javaFiles)
+        }
     }
 }
 
@@ -20,14 +28,19 @@ abstract class JavaJarTask : DefaultTask() {
 
     @TaskAction
     fun createJar() {
-
+        project.exec {
+            commandLine("jar", "cf", outputJar.absolutePath, "-C", inputDir.absolutePath, ".")
+        }
     }
 }
 
 tasks.register<JavaCompileTask>("compile") {
-
+    inputDirLocation = file("./src/main/java")
+    outputDirectory = file("${buildDir}/classes")
 }
 
 tasks.register<JavaJarTask>("createJar") {
-
+    dependsOn("compile")
+    inputDir = tasks.named<JavaCompileTask>("compile").get().outputDirectory
+    outputJar = file("${buildDir}/libs/app.jar")
 }
