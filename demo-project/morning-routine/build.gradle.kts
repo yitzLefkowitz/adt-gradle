970b71a753695659150238d03ec38f15b0696545
Yitz <dlefkowitz@lightricks.com>
2024-06-19 21:30:25 +0300

    Demonstrate task ordering.

---
+++ demo-project/morning-routine/build.gradle.kts
@@ -0,0 +1,36 @@
+// dependsOn
+tasks.register("putOnSocks") {
+    doLast { println("Putting on socks.") }
+}
+
+tasks.register("putOnShoes") {
+    dependsOn("putOnSocks")
+    doLast { println("Putting on shoes.") }
+}
+
+// finalizedBy
+tasks.register("eatBreakfast") {
+    finalizedBy("brushYourTeeth")
+    doLast { println("Eating breakfast.") }
+}
+
+tasks.register("brushYourTeeth") {
+    doLast { println("Brushing teeth.") }
+}
+
+// mustRunAfter
+tasks.register("takeShower") {
+    doLast { println("Taking a shower.") }
+}
+
+tasks.register("putOnDeodorant") {
+    shouldRunAfter("takeShower")
+    doLast{ println("Putting on deodorant.") }
+}
+
+tasks.register("morningRoutine") {
+    dependsOn("takeShower", "putOnShoes", "eatBreakfast")
+}
+
+// Problem - took a shower, after putting on socks and shoes!
+// Problem - took a shower but forgot deodorant
